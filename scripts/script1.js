eb2094512ba09fdc36351c6c1067c8954db80ac4
EC2 Default User <ec2-user@ip-172-31-52-233.ec2.internal>
2022-03-02 20:58:08 +0000

    consolelog and script1.js part1

---
+++ scripts/script1.js
@@ -0,0 +1,26 @@
+// alert("Hello CS Foundations!");
+
+// single line comments
+
+/*
+multi
+line
+comments
+*/
+
+// log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hi Foundations!';
+console.log(greeting);
+
+// Arrays
+console.log([9,8,7,6,5,4,3]);
+var numList = [1,2,3,4];
+console.log(numList);
+
+//Objects
+console.log({name:'Aladdin', grade:9});
+var character = {name:'Jasmine', grade:10};
+console.log(character);
